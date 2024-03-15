95fe40d087e9c3d2e1e218f3f83698a964378051
cecldwll <thaumaturgy101@gmail.com>
2024-03-14 18:30:06 -0600

    starting personal site html

---
+++ personalsite/styles/app.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const parentBgE1 = document.querySelector('.bg');
+    const homeLink = document.querySelector('.container .text a:first-child');
+    const homeSpan = homeLink.querySelector('span');
+    const homeId = homeSpan.getAttribute('data-id');
+    const homeBgE1 = parentBgE1.querySelector(`.id-${homeId}`);
+
+    parentBgE1.querySelectorAll("img").forEach(img => {
+        img.style.display = 'none';
+    });
+    homeBgE1.style.display = 'block';
+    animateImage(homeBgE1);
+
+    const linksEls = document.querySelectorAll('.container .text a');
+    
+    linksEls.forEach(link => {
+        link.addEventListener('mouseover', (e) => {
+            e.preventDefault(); // Prevent default link behavior
+            const id = link.querySelector('span').getAttribute('data-id');
+            const bgE1 = parentBgE1.querySelector(`.id-${id}`);
+
+            parentBgE1.querySelectorAll("img").forEach(img => {
+                img.style.display = 'none';
+            });
+            bgE1.style.display = 'block';
+            animateImage(bgE1);
+        });
+    });
+});
+
+function animateImage(element) {
+    element.style.opacity = '0.6';
+    element.style.transform = 'scale(1)';
+    let opacity = 0.6;
+    let scale = 1;
+
+    const animation = setInterval(() => {
+        opacity += 0.04; // Adjust the increment for smoother animation
+        scale += 0.01; // Adjust the increment for smoother animation
+
+        if (opacity >= 1) {
+            clearInterval(animation);
+            return;
+        }
+
+        element.style.opacity = opacity;
+        element.style.transform = `scale(${scale})`;
+    }, 25); // Adjust the interval for smoother animation
+}
